4a582a323b4cbb4903d146c945e8e1692637c9cb
Talha Abbasi <talhaabbasi2009@hotmail.com>
2022-09-18 12:44:26 +0500

    [FEAT] Save to file :muscle:

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
+const fs = require("fs")
 const express = require("express")
-const app = express()
 const cors = require("cors")
+const app = express()
+
 const port = 80
 
 app.use(cors())
 app.use(express.json())
 
+var dir = "./code"
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
+
 app.post("/javascript", (req, res) => {
+  fs.writeFileSync(`${dir}/test.py`, req.body.code)
   console.log(req.body)
   res.send("Hello World!")
 })
